6bda7994f3f10a9a05f838eca7f670a0526691dd
ricey <truonghoangtampro@gmail.com>
2024-10-12 16:36:26 -0500

    Added more input field

--- app.py
+++ app.py
@@ -9,7 +9,18 @@ def index():
 @app.route('/submit_state', methods=['POST'])
 def submit_state():
     state = request.form['state']
-    return f"Hello, user from {state}!"
+    city = request.form['city']
+    size = request.form['size']
+    cost = request.form['cost']
+
+    # Display a message using the collected inputs
+    return f"""
+        <h1>College Information</h1>
+        <p>State: {state}</p>
+        <p>City: {city}</p>
+        <p>College Size: {size} students</p>
+        <p>Cost of Attendance: ${cost}</p>
+    """
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
