e1b05304241333bf73e8aca580cca717f962b0ee
ricey <truonghoangtampro@gmail.com>
2024-10-12 16:14:47 -0500

    Added question form

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def index():
     return render_template('index.html')
+
+@app.route('/submit_state', methods=['POST'])
+def submit_state():
+    state = request.form['state']
+    return f"Hello, user from {state}!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
