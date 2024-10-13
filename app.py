164cc439ef20c788c36d157750f5cf624b715d39
jackson-pittman <jcp3236@mavs.uta.edu>
2024-10-13 04:00:24 -0500

    Changed size display to show a range

--- app.py
+++ app.py
@@ -2,6 +2,12 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+size_mapping = {
+    "small": {"min": 0, "max": 5000},
+    "medium": {"min": 5000, "max": 15000},
+    "large": {"min": 15000, "max": 100000}  # Setting a large max for large universities
+}
+
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -14,12 +20,15 @@ def submit_state():
     min_cost = request.form['minCost']  # Get minimum cost from the form
     max_cost = request.form['maxCost']  # Get maximum cost from the form
 
+    min_students = size_mapping[size]["min"]
+    max_students = size_mapping[size]["max"]
+
     # Display a message using the collected inputs
     return f"""
         <h1>College Information</h1>
         <p>State: {state}</p>
         <p>City: {city}</p>
-        <p>College Size: {size} </p>
+        <p>College Size: {size.capitalize()} (Student Range: {min_students} - {max_students} students) </p>
         <p>Cost of Attendance Range: ${min_cost} - ${max_cost}</p>
     """
 
